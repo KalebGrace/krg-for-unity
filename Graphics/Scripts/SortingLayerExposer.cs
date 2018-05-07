387e01c9dfde394b4a75090c6ab3b557273e5f8a
Kaleb Grace <code@kalebgrace.com>
2018-05-07 04:50:45 -0700

    Add SortingLayerExposer

---
+++ Graphics/Scripts/SortingLayerExposer.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KRG {
+	
+	[ExecuteInEditMode]
+	public class SortingLayerExposer : MonoBehaviour {
+
+#region serialized fields
+
+		[SerializeField]
+		string _sortingLayerName = "Default";
+		[SerializeField]
+		int _sortingOrder;
+
+#endregion
+
+#region private fields
+
+		MeshRenderer _meshRenderer;
+
+#endregion
+
+#region MonoBehaviour methods
+
+		//WARNING: this function will only be called automatically if playing a GAME BUILD
+		//...it will NOT be called if using the Unity editor
+		void Awake() {
+			UpdateMeshRenderer();
+		}
+
+		//WARNING: this function will only be called automatically if using the UNITY EDITOR
+		//...it will NOT be called if playing a game build
+		void OnValidate() {
+			UpdateMeshRenderer();
+		}
+
+#endregion
+
+#region private methods
+
+		void UpdateMeshRenderer() {
+			if (_meshRenderer == null) {
+				_meshRenderer = GetComponent<MeshRenderer>();
+				if (_meshRenderer == null) {
+					Debug.LogWarning("There is no MeshRenderer on this game object.");
+					return;
+				}
+			}
+			_meshRenderer.sortingLayerName = _sortingLayerName;
+			_meshRenderer.sortingOrder = _sortingOrder;
+		}
+
+#endregion
+
+	}
+}
