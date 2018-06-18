03052a86c2db97714683802231f639d0612f9c88
Kaleb Grace <code@kalebgrace.com>
2018-06-17 19:36:15 -0700

    Add experimental animation to item

--- ItemLoot/Scripts/Item.cs
+++ ItemLoot/Scripts/Item.cs
@@ -2,9 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+#if NS_DG_TWEENING
+using DG.Tweening;
+#endif
+
 namespace KRG {
 
     public class Item : MonoBehaviour {
+
+#region serialized fields
+
+        [Header("VFX")]
+
+        [SerializeField]
+        protected Transform _animatingBody;
+
+#endregion
 
 #region protected fields
 
@@ -13,6 +26,10 @@ namespace KRG {
 #endregion
 
 #region MonoBehaviour methods
+
+        protected virtual void Start() {
+            AnimateBody();
+        }
 
         protected virtual void OnTriggerEnter(Collider other) {
             if (OnCollect(other)) G.End(gameObject);
@@ -37,6 +54,20 @@ namespace KRG {
             var dt = other.GetComponent<DamageTaker>();
             if (dt != null && _itemData.hp > 0) dt.AddHP(_itemData.hp);
             return true;
+        }
+
+#endregion
+
+#region protected methods
+
+        protected virtual void AnimateBody() {
+            if (_animatingBody == null) return;
+#if NS_DG_TWEENING
+            const float duration = 2;
+            const float diameter = 0.2f;
+            var endValue = _animatingBody.localPosition + Vector3.up * diameter;
+            _animatingBody.DOLocalMove(endValue, duration).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
+#endif
         }
 
 #endregion
