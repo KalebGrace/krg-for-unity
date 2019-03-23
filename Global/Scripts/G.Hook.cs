026c2a859ec5a50be71d671643adf90cb3bb2f9d
k76 <krg0077++src@gmail.com>
2019-03-23 16:31:35 -0700

    Add Hook class & FungusSayOnEnterDisplayText

---
+++ Global/Scripts/G.Hook.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KRG
+{
+    partial class G
+    {
+        public static class Hook
+        {
+            /// ----------
+            ///    CALL
+            /// ----------
+
+            public static void Call(ID hookID, ref string s)
+            {
+                switch (hookID)
+                {
+                    case ID.FungusSayOnEnterDisplayText:
+                        var e = FungusSayOnEnterDisplayText;
+                        if (e != null) e(ref s);
+                        break;
+                    default:
+                        G.U.Unsupported(null, hookID);
+                        break;
+                }
+            }
+
+
+            /// ----------
+            ///   EVENT!
+            /// ----------
+
+            public static event Delegate.RefString FungusSayOnEnterDisplayText;
+
+
+            /// ----------
+            ///  DELEGATE
+            /// ----------
+
+            public static class Delegate
+            {
+                public delegate void RefString(ref string s);
+            }
+
+
+            /// ----------
+            ///     ID
+            /// ----------
+
+            public enum ID
+            {
+                //f -> 6 * 3 = 18, u -> 21 * 3 = 63, & then 5000
+                FungusSayOnEnterDisplayText = 18635000,
+            }
+        }
+    }
+}
