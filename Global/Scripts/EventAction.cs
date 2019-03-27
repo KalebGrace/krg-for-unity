d58259d2f4022bd8f837d66f52a0a85fbfbbf7a0
k76 <krg0077++src@gmail.com>
2019-03-27 00:31:48 -0700

    Add EventAction structs for simple priority-based events

---
+++ Global/Scripts/EventAction.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace KRG
+{
+    public struct EventAction
+    {
+        event Action actionHigh;
+        event Action actionNormal;
+
+        public void AddHigh(Action action)
+        {
+            if (actionHigh != null)
+            {
+                G.U.Warning("There is already a high priority action for this event.");
+            }
+            actionHigh += action;
+        }
+
+        public void Add(Action action)
+        {
+            actionNormal += action;
+        }
+
+        public void Remove(Action action)
+        {
+            actionHigh -= action;
+            actionNormal -= action;
+        }
+
+        public void Invoke()
+        {
+            if (actionHigh != null) actionHigh();
+            if (actionNormal != null) actionNormal();
+        }
+    }
+
+    public struct EventAction<T>
+    {
+        event Action<T> actionHigh;
+        event Action<T> actionNormal;
+
+        public void AddHigh(Action<T> action)
+        {
+            if (actionHigh != null)
+            {
+                G.U.Warning("There is already a high priority action for this event.");
+            }
+            actionHigh += action;
+        }
+
+        public void Add(Action<T> action)
+        {
+            actionNormal += action;
+        }
+
+        public void Remove(Action<T> action)
+        {
+            actionHigh -= action;
+            actionNormal -= action;
+        }
+
+        public void Invoke(T in1)
+        {
+            if (actionHigh != null) actionHigh(in1);
+            if (actionNormal != null) actionNormal(in1);
+        }
+    }
+}
