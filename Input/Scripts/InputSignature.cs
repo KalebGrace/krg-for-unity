d76e39dc309d92616df6b4910209893e9b5ac766
k76 <krg0077++src@gmail.com>
2019-04-01 04:12:28 -0700

    Temporarily disable Enum attribute for InputSignature

--- Input/Scripts/InputSignature.cs
+++ Input/Scripts/InputSignature.cs
@@ -7,14 +7,16 @@ namespace KRG {
 
     public abstract class InputSignature : ScriptableObject {
 
-        [Enum(typeof(InputCommand))]
+        //TODO: custom "Enum" attribute on an int array doesn't play well w/ Odin Inspector
+        [Header("Obsolete KRG InputSignature")]
+        //[Enum(typeof(InputCommand))]
         [SerializeField]
         [FormerlySerializedAs("m_inputCommands")]
         protected int[] _inputCommands;
 
         string _key;
 
-        public int complexity { get { return _inputCommands.Length; } }
+        public virtual int complexity { get { return _inputCommands.Length; } }
 
         public abstract bool isExecuted { get; }
 
@@ -22,7 +24,7 @@ namespace KRG {
         /// Gets the key, a unique identifier. It will be cached upon first acesss.
         /// </summary>
         /// <value>The key.</value>
-        public string key {
+        public virtual string key {
             get {
                 if (string.IsNullOrEmpty(_key)) {
                     if (G.U.Assert(_inputCommands != null)) {
