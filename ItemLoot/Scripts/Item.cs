a8d1de1ae6614d1b180fc4977231a3908408ba96
k76 <krg0077++src@gmail.com>
2019-04-20 18:19:34 -0700

    Clean up Item a bit; Remove default item animation

--- ItemLoot/Scripts/Item.cs
+++ ItemLoot/Scripts/Item.cs
@@ -1,54 +1,51 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 #if NS_DG_TWEENING
 using DG.Tweening;
 #endif
 
-namespace KRG {
-
-    public class Item : MonoBehaviour {
-
-#region serialized fields
-
+namespace KRG
+{
+    public class Item : MonoBehaviour
+    {
         [Header("VFX")]
 
         [SerializeField]
         protected Transform _animatingBody;
 
-#endregion
-
-#region protected fields
 
         protected ItemData _itemData;
 
-#endregion
 
-#region MonoBehaviour methods
-
-        protected virtual void Start() {
-            AnimateBody();
+        protected virtual void Start()
+        {
+            if (_animatingBody != null) StartAnimateBody();
         }
 
-        protected virtual void OnTriggerEnter(Collider other) {
+        protected virtual void OnDestroy()
+        {
+            if (_animatingBody != null) EndAnimateBody();
+        }
+
+        protected virtual void OnTriggerEnter(Collider other)
+        {
             if (OnCollect(other)) G.End(gameObject);
         }
 
-#endregion
 
-#region public methods
-
-        public virtual void Init(ItemData itemData) {
+        public virtual void Init(ItemData itemData)
+        {
             _itemData = itemData;
         }
 
-        public virtual bool OnCollect(Collider other) {
+        public virtual bool OnCollect(Collider other)
+        {
             //TODO: differentiate between inventory items and instant-use items
             return OnUse(other);
         }
 
-        public virtual bool OnUse(Collider other) {
+        public virtual bool OnUse(Collider other)
+        {
             //TODO: differentiate between the character, etc. using the item
             if (other.gameObject.tag != "Player") return false;
             var dt = other.GetComponent<DamageTaker>();
@@ -56,21 +53,12 @@ namespace KRG {
             return true;
         }
 
-#endregion
-
-#region protected methods
-
-        protected virtual void AnimateBody() {
-            if (_animatingBody == null) return;
-#if NS_DG_TWEENING
-            const float duration = 2;
-            const float diameter = 0.2f;
-            var endValue = _animatingBody.localPosition + Vector3.up * diameter;
-            _animatingBody.DOLocalMove(endValue, duration).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
-#endif
+        protected virtual void StartAnimateBody()
+        {
         }
 
-#endregion
-
+        protected virtual void EndAnimateBody()
+        {
+        }
     }
 }
