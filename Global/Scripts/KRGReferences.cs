9a2d4e9bd3698a491a79225c9a5715441e1852b7
k76 <krg0077++src@gmail.com>
2019-04-30 03:09:30 -0700

    Update TextMesh Pro refs to Package Manager version
    - Create clones of "TMProPaid" prefabs, except using the Package Manager script references
    - Update KRGReferences to use these if the KRG_X_TMPRO define is set

--- Global/Scripts/KRGReferences.cs
+++ Global/Scripts/KRGReferences.cs
@@ -1,29 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-namespace KRG {
-
-    //[CreateAssetMenu]
-    public sealed class KRGReferences : ScriptableObject {
-        
-#region PROPERTIES
-
+namespace KRG
+{
+    public sealed class KRGReferences : ScriptableObject
+    {
         public CharacterDebugText characterDebugTextPrefab { get; private set; }
 
         public RasterAnimationInfo rasterAnimationInfoPrefab { get; private set; }
 
-#endregion
-
-#region METHODS: ScriptableObject
-
-        void Awake() {
-#if NS_TMPRO_PAID
-            //these are only for the old PAID version of TextMesh Pro
+        void Awake()
+        {
+#if KRG_X_TMPRO
+            //these are only for the new Unity Package Manager version of TextMesh Pro
+            characterDebugTextPrefab = Resources.Load<CharacterDebugText>("Character/CharacterDebugTextTMPro");
+            rasterAnimationInfoPrefab = Resources.Load<RasterAnimationInfo>("Graphics/RasterAnimationInfoTMPro");
+#elif NS_TMPRO_PAID
+            //these are only for the OLD PAID Asset Store version of TextMesh Pro
             characterDebugTextPrefab = Resources.Load<CharacterDebugText>("Character/CharacterDebugTextTMProPaid");
             rasterAnimationInfoPrefab = Resources.Load<RasterAnimationInfo>("Graphics/RasterAnimationInfoTMProPaid");
 #elif NS_TMPRO
-			//these are only for the new FREE version of TextMesh Pro
+			//these are only for the OLD FREE Asset Store version of TextMesh Pro
 			characterDebugTextPrefab = Resources.Load<CharacterDebugText>("Character/CharacterDebugTextTMProFree");
 			rasterAnimationInfoPrefab = Resources.Load<RasterAnimationInfo>("Graphics/RasterAnimationInfoTMProFree");
 #else
@@ -31,8 +27,5 @@ namespace KRG {
             rasterAnimationInfoPrefab = Resources.Load<RasterAnimationInfo>("Graphics/RasterAnimationInfo");
 #endif
         }
-
-#endregion
-
     }
 }
