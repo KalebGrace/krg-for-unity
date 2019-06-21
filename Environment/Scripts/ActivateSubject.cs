3a31dbe3a6b0599d7aa430ed09b0422f5217cf9a
k76 <krg0077++src@gmail.com>
2019-06-21 03:34:40 -0700

    Add proper ActivateSubject MonoBehaviour (for reference)

---
+++ Environment/Scripts/ActivateSubject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace KRG
+{
+    public class ActivateSubject : MonoBehaviour
+    {
+        private IActivate m_Interface;
+
+        public IActivate Interface => m_Interface ?? LazyInitializeInterface();
+
+        private IActivate LazyInitializeInterface()
+        {
+            m_Interface = GetComponent<IActivate>();
+            return m_Interface;
+        }
+    }
+}
