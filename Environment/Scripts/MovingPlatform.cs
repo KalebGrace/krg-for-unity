da85f0053c28ba9b2fe0afdf5e36fcddeabc5f6c
k76 <krg0077++src@gmail.com>
2019-06-29 21:58:50 -0700

    Add Breakable obj and MovingPlatform environment scripts

---
+++ Environment/Scripts/MovingPlatform.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KRG
+{
+    public class MovingPlatform : MonoBehaviour
+    {
+        public MeshRenderer meshRenderer;
+        public Material deactivatedMaterial;
+        public Material activatedMaterial;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
