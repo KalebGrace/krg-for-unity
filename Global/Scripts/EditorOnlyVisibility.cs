0add74acafe4530816334f6892a0de3f715905b0
k76 <krg0077++src@gmail.com>
2019-07-18 22:16:25 -0700

    Add EditorOnlyVisibility script/material

---
+++ Global/Scripts/EditorOnlyVisibility.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace KRG
+{
+    [ExecuteInEditMode]
+    public class EditorOnlyVisibility : MonoBehaviour
+    {
+        private BoxCollider m_BoxCollider;
+        private Renderer m_Renderer;
+
+        private void OnValidate()
+        {
+            m_Renderer = GetComponent<Renderer>();
+
+            if (m_Renderer != null && m_Renderer.sharedMaterial.name == "Default-Material")
+            {
+                m_Renderer.sharedMaterial = Resources.Load<Material>("Global/EditorOnlyVisibilityMaterial");
+            }
+        }
+
+        private void Awake()
+        {
+            m_BoxCollider = GetComponent<BoxCollider>();
+            m_Renderer = GetComponent<Renderer>();
+
+            if (m_Renderer != null)
+            {
+                m_Renderer.enabled = G.isInEditMode;
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (m_BoxCollider != null)
+            {
+                Gizmos.color = Color.magenta;
+                Gizmos.DrawWireCube(transform.position + m_BoxCollider.center, m_BoxCollider.size);
+            }
+        }
+    }
+}
