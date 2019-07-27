c04db63789d9de3e88ccd95b430e85ab89e4a747
k76 <krg0077++src@gmail.com>
2019-07-27 16:41:37 -0700

    Modernize Character script

--- Character/Scripts/Character.cs
+++ Character/Scripts/Character.cs
@@ -1,60 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Serialization;
+﻿using UnityEngine;
 
-namespace KRG {
-
-    public abstract class Character : MonoBehaviour, IStateOwner {
-
-
+namespace KRG
+{
+    public abstract class Character : MonoBehaviour, IStateOwner
+    {
         /// <summary>
-        /// A VisRect GameObject is an advanced form of the old "Center" GameObject.
-        /// It serves the same purpose, but also has added functionality.
+        /// The child VisRect GameObject should be positioned at the visual center of the character.
+        /// By contrast, the root Character GameObject should be positioned under the character's feet.
         /// </summary>
         [SerializeField]
-        [FormerlySerializedAs("m_visRect")]
         protected VisRect _visRect;
 
-        /// <summary>
-        /// DEPRECATED:
-        /// A child "Center" GameObject that's positioned at the visual center of the character.
-        /// By contrast, the current GameObject should be positioned under the character's feet.
-        /// </summary>
-        [SerializeField]
-        [FormerlySerializedAs("m_center")]
-        protected GameObject _center;
-
-
-        GraphicsController _graphicsController;
-
+        [System.NonSerialized]
         protected Transform _transform;
 
-
-        public GraphicsController graphicsController { get { return _graphicsController; } }
+        public GraphicsController GraphicsController { get; private set; }
 
         public abstract CharacterType Type { get; }
 
-        public VisRect visRect { get { return _visRect; } }
+        public VisRect VisRect => _visRect;
 
-
-        protected virtual void Awake() {
-            if (_visRect != null) {
-                if (_center != null) {
-                    G.U.Warning("When a VisRect is assigned to this Component, "
-                    + "a manual \"Center\" GameObject assignment is unnecessary, and will be ignored.");
-                }
-                _center = _visRect.gameObject;
-            } else {
-                G.U.Require(_center, "VisRect (or \"Center\" GameObject)");
-            }
-            _graphicsController = this.Require<GraphicsController>();
+        protected virtual void Awake()
+        {
             _transform = transform;
+            G.U.Require(_visRect, "VisRect GameObject");
+            GraphicsController = this.Require<GraphicsController>();
 
 #if DEBUG_VISIBILITY
+            Transform center = _visRect.transform;
             KRGReferences refs = G.config.krgReferences;
-            Instantiate(refs.characterDebugTextPrefab, _visRect.transform).Init(this);
-            _graphicsController.rasterAnimationInfo = Instantiate(refs.rasterAnimationInfoPrefab, _visRect.transform);
+            Instantiate(refs.characterDebugTextPrefab, center).Init(this);
+            GraphicsController.rasterAnimationInfo = Instantiate(refs.rasterAnimationInfoPrefab, center);
 #endif
         }
     }
