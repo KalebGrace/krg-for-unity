a2845263b08b6210717952cc91beba9401b44180
k76 <krg0077++src@gmail.com>
2019-07-26 19:14:36 -0700

    Fix DamageValue

--- Damage/Scripts/DamageValue.cs
+++ Damage/Scripts/DamageValue.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -7,85 +7,48 @@ using UnityEngine.Serialization;
 using DG.Tweening;
 #endif
 
-#if KRG_X_TMPRO || NS_TMPRO_PAID || NS_TMPRO
-using TMPro;
-#endif
+namespace KRG
+{
 
-namespace KRG {
-
-    public class DamageValue : MonoBehaviour {
+    public class DamageValue : MonoBehaviour
+    {
 
 #pragma warning disable 0414
         [Enum(typeof(TimeThreadInstance))]
         [SerializeField]
-        [FormerlySerializedAs("m_timeThreadIndex")]
         int _timeThreadIndex = (int)TimeThreadInstance.UseDefault;
 #pragma warning restore 0414
 
-#if KRG_X_TMPRO || NS_TMPRO_PAID || NS_TMPRO
         [SerializeField]
-        [FormerlySerializedAs("m_text100")]
-		TMP_Text _text100 = default;
+        TMP_Text _text100 = default;
 
         [SerializeField]
-        [FormerlySerializedAs("m_text010")]
-		TMP_Text _text010 = default;
+        TMP_Text _text010 = default;
 
         [SerializeField]
-        [FormerlySerializedAs("m_text001")]
-		TMP_Text _text001 = default;
-
-#else
-        [SerializeField]
-        [FormerlySerializedAs("m_font")]
-        Font _font = default;
-
-        [SerializeField]
-        [FormerlySerializedAs("m_material")]
-        Material _material = default;
-
-        [SerializeField]
-        [FormerlySerializedAs("m_text100")]
-        TextMesh _text100 = default;
-
-        [SerializeField]
-        [FormerlySerializedAs("m_text010")]
-        TextMesh _text010 = default;
-
-        [SerializeField]
-        [FormerlySerializedAs("m_text001")]
-        TextMesh _text001 = default;
-#endif
+        TMP_Text _text001 = default;
 
 #if NS_DG_TWEENING
         IEnd _target;
         List<Sequence> _tweens = new List<Sequence>();
 
-        ITimeThread timeThread {
-            get {
+        ITimeThread timeThread
+        {
+            get
+            {
                 return G.time.GetTimeThread(_timeThreadIndex, TimeThreadInstance.Gameplay);
             }
         }
 
-        void Awake() {
-#if !(KRG_X_TMPRO || NS_TMPRO_PAID || NS_TMPRO)
-            _text001.font = _font;
-            _text010.font = _font;
-            _text100.font = _font;
-
-            _text001.GetComponent<Renderer>().sharedMaterial = _material;
-            _text010.GetComponent<Renderer>().sharedMaterial = _material;
-            _text100.GetComponent<Renderer>().sharedMaterial = _material;
-#endif
-        }
-
-        public void Init(IEnd target, int damage) {
+        public void Init(IEnd target, int damage)
+        {
             _target = target;
             _target.end.actions += OnTargetDestroy;
 
             string d = damage.ToString();
             int l = d.Length;
-            switch (l) {
+            switch (l)
+            {
                 case 1:
                     _text100.gameObject.SetActive(false);
                     _text010.gameObject.SetActive(false);
@@ -132,18 +95,16 @@ namespace KRG {
             }
         }
 
-#if KRG_X_TMPRO || NS_TMPRO_PAID || NS_TMPRO
-        void Animate(TMP_Text textMesh, float heightMult) {
-#else
-        void Animate(TextMesh textMesh, float heightMult) {
-#endif
+        void Animate(TMP_Text textMesh, float heightMult)
+        {
             Sequence s = DOTween.Sequence();
             Transform textTf = textMesh.transform;
 
             //Bounce.
             float h = 5f * heightMult;
             float t = 16f / 30f; //Total bounce time will actually be 15f / 30f.
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < 4; i++)
+            {
                 s.Append(textTf.DOLocalMoveY(h /= 2f, t /= 2f).SetEase(Ease.OutQuad));
                 s.Append(textTf.DOLocalMoveY(0f, t).SetEase(Ease.InQuad));
             }
@@ -157,11 +118,7 @@ namespace KRG {
             s.Append(textTf.DOLocalMoveX(0f, 4f / 30f).SetEase(Ease.Linear));
 
             //Darken (at same time).
-#if KRG_X_TMPRO || NS_TMPRO_PAID || NS_TMPRO
             s.Join(textMesh.DOColor(Color.black, 4f / 30f).SetEase(Ease.Linear));
-#else
-            s.Join(textMesh.GetComponent<Renderer>().material.DOColor(Color.black, 4f / 30f).SetEase(Ease.Linear));
-#endif
 
             //Wait.
             s.AppendInterval(2f / 30f);
@@ -174,32 +131,40 @@ namespace KRG {
             timeThread.AddTween(s);
         }
 
-        void Dispose() {
+        void Dispose()
+        {
             gameObject.Dispose();
         }
 
-        void OnDestroy() {
-            if (_target != null) {
+        void OnDestroy()
+        {
+            if (_target != null)
+            {
                 _target.end.actions -= OnTargetDestroy;
             }
 
             //some of the following may be null if the scene is changing or the app is quitting
-            if (_text100 != null && _text100.gameObject.activeSelf) {
+            if (_text100 != null && _text100.gameObject.activeSelf)
+            {
                 DestroyImmediate(_text100.GetComponent<Renderer>().material);
             }
-            if (_text010 != null && _text010.gameObject.activeSelf) {
+            if (_text010 != null && _text010.gameObject.activeSelf)
+            {
                 DestroyImmediate(_text010.GetComponent<Renderer>().material);
             }
-            if (_text001 != null) {
+            if (_text001 != null)
+            {
                 DestroyImmediate(_text001.GetComponent<Renderer>().material);
             }
 
-            foreach (Sequence s in _tweens) {
+            foreach (Sequence s in _tweens)
+            {
                 timeThread.RemoveTween(s);
             }
         }
 
-        void OnTargetDestroy() {
+        void OnTargetDestroy()
+        {
             _target = null;
             transform.parent = null; //This needs to live on even if the target is destroyed.
 
@@ -207,7 +172,8 @@ namespace KRG {
             //TODO: find out why this happens (it happens in SoAm.TestDamageRing as well)
             //...and create a common solution for it
             var cfb = GetComponent<CameraFacingBillboard>();
-            if (cfb != null) {
+            if (cfb != null)
+            {
                 cfb.enabled = true;
             }
         }
