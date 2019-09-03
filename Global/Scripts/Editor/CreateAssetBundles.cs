4aff0fb89d24b1fad7e757a31ab151ae4f5381ac
k76 <krg0077++src@gmail.com>
2019-09-02 23:47:39 -0700

    Add GameObjectEntity, SpriteLogic, & more

---
+++ Global/Scripts/Editor/CreateAssetBundles.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using System.IO;
+
+namespace KRG
+{
+    public class CreateAssetBundles
+    {
+        [MenuItem("Assets/Build AssetBundles")]
+        static void BuildAllAssetBundles()
+        {
+            string assetBundleDirectory = "Assets/StreamingAssets";
+            if (!Directory.Exists(assetBundleDirectory))
+            {
+                Directory.CreateDirectory(assetBundleDirectory);
+            }
+            var bt = BuildTarget.StandaloneOSX;
+            BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, bt);
+
+            G.U.Log("Asset bundles built. If you don't see your bundle, make sure the assets are properly assigned to it in the bottom of the inspector.");
+        }
+    }
+}
---
+++ Object/Scripts/GameObjectEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KRG
+{
+    public class GameObjectEntity : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObjectEntityType m_Type = default;
+
+
+    }
+}
