4af493352a0e48aae67da942410ec76518db1289
k76 <krg0077++src@gmail.com>
2019-11-19 01:18:27 -0800

    G.version = 12;

--- Global/Scripts/G.Core.cs
+++ Global/Scripts/G.Core.cs
@@ -12,7 +12,7 @@ namespace KRG
         /// <summary>
         /// The version of G.
         /// </summary>
-        public const int version = 11;
+        public const int version = 12;
 
         /// <summary>
         /// The cached config (KRGConfig).
