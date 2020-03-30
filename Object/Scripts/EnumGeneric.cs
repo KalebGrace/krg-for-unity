f9976a14854e55e23d711558189456d3ace7cb72
k76 <krg0077++src@gmail.com>
2020-03-30 14:53:55 -0700

    if game-specific enum not found, fall back to KRG

--- Object/Scripts/EnumGeneric.cs
+++ Object/Scripts/EnumGeneric.cs
@@ -4,10 +4,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-namespace KRG {
+namespace KRG
+{
 
     [Serializable]
-    public class EnumGeneric {
+    public class EnumGeneric
+    {
 
         [SerializeField]
         [FormerlySerializedAs("m_intValue")]
@@ -33,13 +35,17 @@ namespace KRG {
 
         public int intValue { get { return _intValue; } }
 
-        public Type type {
-            get {
+        public Type type
+        {
+            get
+            {
                 G.U.Assert(hasType, "The type is null. (If you wish to do a null compare, use hasType instead.)");
                 return _type;
             }
-            set {
-                if (AssertIsEnum(value)) {
+            set
+            {
+                if (AssertIsEnum(value))
+                {
                     _stringType = value.ToString();
                     _type = value;
                     names = Enum.GetNames(value);
@@ -52,92 +58,127 @@ namespace KRG {
         public Array values { get; private set; }
 
 
-        public EnumGeneric(string enumType) {
+        public EnumGeneric(string enumType)
+        {
             Type t = GetType(enumType);
-            if (t != null) {
+            if (t != null)
+            {
                 type = t;
             }
         }
 
-        public EnumGeneric(Type enumType) {
+        public EnumGeneric(Type enumType)
+        {
             type = enumType;
         }
 
-        public EnumGeneric(Type enumType, int intValue) {
+        public EnumGeneric(Type enumType, int intValue)
+        {
             type = enumType;
             _intValue = intValue;
         }
 
-        public bool HasSequentialValues(int startValue, int increment) {
+        public bool HasSequentialValues(int startValue, int increment)
+        {
             return HasSequentialValues(values, startValue, increment);
         }
 
-        public static bool HasSequentialValues(Type enumType, int startValue, int increment) {
-            if (AssertIsEnum(enumType)) {
+        public static bool HasSequentialValues(Type enumType, int startValue, int increment)
+        {
+            if (AssertIsEnum(enumType))
+            {
                 Array vals = Enum.GetValues(enumType);
                 return HasSequentialValues(vals, startValue, increment);
-            } else {
+            }
+            else
+            {
                 return false;
             }
         }
 
-        static bool HasSequentialValues(Array vals, int startValue, int increment) {
+        static bool HasSequentialValues(Array vals, int startValue, int increment)
+        {
             int len = vals.Length;
-            if (len == 0) {
+            if (len == 0)
+            {
                 return false;
             }
             bool match;
-            for (int i = 0; i < len; i++) {
+            for (int i = 0; i < len; i++)
+            {
                 match = false;
-                foreach (var value in vals) {
-                    if ((int)value == startValue + increment * i) {
+                foreach (var value in vals)
+                {
+                    if ((int)value == startValue + increment * i)
+                    {
                         match = true;
                         break;
                     }
                 }
-                if (!match) {
+                if (!match)
+                {
                     return false;
                 }
             }
             return true;
         }
 
-        static Type GetType(string enumType) {
-            if (string.IsNullOrEmpty(enumType)) {
+        static Type GetType(string enumType)
+        {
+            if (string.IsNullOrEmpty(enumType))
+            {
                 G.U.Err("The type is null or empty. The type needs to be an enum (enumeration).");
                 return null;
             }
             Type t = System.Reflection.Assembly.GetExecutingAssembly().GetType(enumType);
-            G.U.Assert(t != null, string.Format("The type \"{0}\" cannot be found.", enumType));
+            if (t == null)
+            {
+                var parts = enumType.Split('.');
+                string enumTypeKRG = "KRG." + parts[parts.Length - 1];
+                t = System.Reflection.Assembly.GetExecutingAssembly().GetType(enumTypeKRG);
+                G.U.Assert(t != null, string.Format("The type \"{0}\" cannot be found.", enumType));
+            }
             return t;
         }
 
-        public Enum ToEnum(int value) {
+        public Enum ToEnum(int value)
+        {
             return ToEnum(_type, value);
         }
 
-        public static Enum ToEnum(string enumType, int value) {
+        public static Enum ToEnum(string enumType, int value)
+        {
             Type t = GetType(enumType);
             return t != null ? ToEnum(t, value) : null;
         }
 
-        public static Enum ToEnum(Type enumType, int value) {
-            if (AssertIsEnum(enumType)) {
+        public static Enum ToEnum(Type enumType, int value)
+        {
+            if (AssertIsEnum(enumType))
+            {
                 object o = Enum.ToObject(enumType, value);
                 return (Enum)o;
-            } else {
+            }
+            else
+            {
                 return null;
             }
         }
 
-        static bool AssertIsEnum(Type enumType) {
-            if (enumType == null) {
+        static bool AssertIsEnum(Type enumType)
+        {
+            if (enumType == null)
+            {
                 G.U.Err("The type is null. The type needs to be an enum (enumeration).");
                 return false;
-            } else if (!enumType.IsEnum) {
+            }
+            else if (!enumType.IsEnum)
+            {
                 G.U.Err("The type \"{0}\" is invalid. The type needs to be an enum (enumeration).", enumType);
                 return false;
-            } else {
+            }
+            else
+            {
                 return true;
             }
         }
