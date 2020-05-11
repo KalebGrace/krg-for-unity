54e18b70000c3771654c0acef5d4cf6e33cd8cda
k76 <krg0077++src@gmail.com>
2020-05-10 22:18:36 -0700

    add EnvironmentChart script

---
+++ Environment/Scripts/EnvironmentChart.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace KRG
+{
+    [CreateAssetMenu(
+        fileName = "SomeWhere_EnvironmentChart.asset",
+        menuName = "KRG Scriptable Object/EnvironmentChart",
+        order = 503
+    )]
+    public class EnvironmentChart : ScriptableObject
+    {
+        public const string ENVIRONMENT_CHART_SUFFIX = "_EnvironmentChart";
+
+        [Header("Game Object Data")]
+
+        [ReadOnly]
+        public string FileName;
+
+        [Enum(typeof(SceneName))]
+        public int EnvironmentID;
+
+        private void OnValidate()
+        {
+            FileName = name.Replace(ENVIRONMENT_CHART_SUFFIX, "");
+        }
+
+        public static string GetBundleName(int environmentID)
+        {
+            return "_e" + environmentID.ToString("D5");
+        }
+    }
+}
