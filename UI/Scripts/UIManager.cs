9dfa653a77b45fb13cebc3c9d17363cb5462e3b5
k76 <krg0077++src@gmail.com>
2020-05-17 20:11:21 -0700

    make color overlay methods use completable/lockable sequences

--- UI/Scripts/UIManager.cs
+++ UI/Scripts/UIManager.cs
@@ -14,8 +14,9 @@ namespace KRG
         // FIELDS
 
         protected GameObject _colorOverlay;
+        protected bool _colorOverlayIsLocked;
 #if NS_DG_TWEENING
-        protected Tweener _colorOverlayTweener;
+        protected Sequence _colorOverlaySequence;
 #endif
 
         private Stack<string> m_Screens = new Stack<string>();
@@ -59,13 +60,63 @@ namespace KRG
 
         // COLOR OVERLAY METHODS
 
+#if NS_DG_TWEENING
         /// <summary>
-        /// Adds a color overlay to the active scene. This creates both a high sort order Canvas, and a child Image.
+        /// Fades a color overlay from color 1 to color 2.
+        /// </summary>
+        /// <returns>The Sequence object used for the fade.</returns>
+        /// <param name="color1">Color to fade out/from.</param>
+        /// <param name="color2">Color to fade in/to.</param>
+        /// <param name="duration">Duration in seconds.</param>
+        /// <param name="lockDuringFade">If true, no further color overlay methods will work until the duration has elapsed.</param>
+        public virtual Sequence FadeColorOverlay(Color color1, Color color2, float duration, bool lockDuringFade = false)
+        {
+            if (_colorOverlayIsLocked) return DOTween.Sequence().AppendInterval(duration);
+            var i = ReplaceColorOverlay(color1);
+            _colorOverlayIsLocked = lockDuringFade;
+            _colorOverlaySequence = DOTween.Sequence().Append(i.DOColor(color2, duration));
+            if (lockDuringFade) _colorOverlaySequence.AppendCallback(() => _colorOverlayIsLocked = false);
+            return _colorOverlaySequence;
+        }
+
+        /// <summary>
+        /// Fades from a color (fades out a color overlay).
+        /// </summary>
+        /// <returns>The Sequence object used for the fade.</returns>
+        /// <param name="color">Color to fade out/from.</param>
+        /// <param name="duration">Duration in seconds.</param>
+        /// <param name="lockDuringFade">If true, no further color overlay methods will work until the duration has elapsed.</param>
+        public virtual Sequence FadeFromColor(Color color, float duration, bool lockDuringFade = false)
+        {
+            Color clear = color.SetAlpha(0); // transparent clone of color
+            return FadeColorOverlay(color, clear, duration, lockDuringFade);
+        }
+
+        /// <summary>
+        /// Fades to a color (fades in a color overlay).
+        /// </summary>
+        /// <returns>The Sequence object used for the fade.</returns>
+        /// <param name="color">Color to fade in/to.</param>
+        /// <param name="duration">Duration in seconds.</param>
+        /// <param name="lockDuringFade">If true, no further color overlay methods will work until the duration has elapsed.</param>
+        public virtual Sequence FadeToColor(Color color, float duration, bool lockDuringFade = false)
+        {
+            Color clear = color.SetAlpha(0); // transparent clone of color
+            return FadeColorOverlay(clear, color, duration, lockDuringFade);
+        }
+#endif
+
+        /// <summary>
+        /// Replaces the color overlay on the active scene,
+        /// or simply adds an overlay if one does not yet exist.
+        /// This function creates both a high sort order Canvas, and a child Image.
         /// </summary>
         /// <returns>The newly created Image component used to display the provided color.</returns>
         /// <param name="color">Color.</param>
-        public virtual Image AddColorOverlay(Color color)
+        public virtual Image ReplaceColorOverlay(Color color)
         {
+            if (_colorOverlayIsLocked) return null;
+
             RemoveColorOverlay();
 
             _colorOverlay = new GameObject("ColorOverlayCanvas", typeof(Canvas));
@@ -84,58 +135,17 @@ namespace KRG
             return i;
         }
 
-#if NS_DG_TWEENING
-        /// <summary>
-        /// Fades a color overlay from color 1 to color 2.
-        /// </summary>
-        /// <returns>The Tweener object used for the fade.</returns>
-        /// <param name="color1">Color 1.</param>
-        /// <param name="color2">Color 2.</param>
-        /// <param name="duration">Duration in seconds.</param>
-        public virtual Tweener FadeColorOverlay(Color color1, Color color2, float duration)
-        {
-            var i = AddColorOverlay(color1);
-            _colorOverlayTweener = i.DOColor(color2, duration);
-            return _colorOverlayTweener;
-        }
-
-        /// <summary>
-        /// Fades in a color overlay.
-        /// </summary>
-        /// <returns>The Tweener object used for the fade.</returns>
-        /// <param name="color">Color.</param>
-        /// <param name="duration">Duration in seconds.</param>
-        public virtual Tweener FadeInColorOverlay(Color color, float duration)
-        {
-            var i = AddColorOverlay(color.SetAlpha(0)); //use transparent clone of color
-            _colorOverlayTweener = i.DOFade(color.a, duration);
-            return _colorOverlayTweener;
-        }
-
-        /// <summary>
-        /// Fades out a color overlay.
-        /// </summary>
-        /// <returns>The Tweener object used for the fade.</returns>
-        /// <param name="color">Color.</param>
-        /// <param name="duration">Duration in seconds.</param>
-        public virtual Tweener FadeOutColorOverlay(Color color, float duration)
-        {
-            var i = AddColorOverlay(color);
-            _colorOverlayTweener = i.DOFade(0, duration);
-            return _colorOverlayTweener;
-        }
-#endif
-
         /// <summary>
         /// Removes the color overlay from the canvas.
         /// </summary>
         public virtual void RemoveColorOverlay()
         {
+            if (_colorOverlayIsLocked) return;
 #if NS_DG_TWEENING
-            if (_colorOverlayTweener != null)
+            if (_colorOverlaySequence != null)
             {
-                _colorOverlayTweener.Kill();
-                _colorOverlayTweener = null;
+                _colorOverlaySequence.Complete(true);
+                _colorOverlaySequence = null;
             }
 #endif
             if (_colorOverlay != null)
