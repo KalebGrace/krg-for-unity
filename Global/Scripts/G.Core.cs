c8475c1f6b10235edb8257796dd1d580168193b0
k76 <krg0077++src@gmail.com>
2020-08-28 19:17:39 -0700

    G.version = 29;

--- Global/Scripts/G.Core.cs
+++ Global/Scripts/G.Core.cs
@@ -12,7 +12,7 @@ namespace KRG
         /// <summary>
         /// The version of G.
         /// </summary>
-        public const int version = 28;
+        public const int version = 29;
 
         /// <summary>
         /// Fires during G's Awake method.
