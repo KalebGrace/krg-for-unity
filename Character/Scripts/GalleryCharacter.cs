7f489edf0070b5efabf01988743baffbcfa9a897
k76 <krg0077++src@gmail.com>
2020-09-08 05:47:51 -0700

    Add gallery character script

---
+++ Character/Scripts/GalleryCharacter.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace KRG
+{
+    [ExecuteAlways]
+    public class GalleryCharacter : MonoBehaviour
+    {
+        public Texture2D CharacterTexture;
+
+        private void Start()
+        {
+            UpdateTexture();
+        }
+
+        private void OnValidate()
+        {
+            UpdateTexture();
+        }
+
+        private void UpdateTexture()
+        {
+            var r = GetComponent<MeshRenderer>();
+            if (G.U.IsEditMode(this))
+            {
+                var m = r.sharedMaterial;
+                m.mainTexture = CharacterTexture;
+                r.sharedMaterial = m;
+            }
+            else
+            {
+                var m = r.material;
+                m.mainTexture = CharacterTexture;
+                r.material = m;
+            }
+        }
+    }
+}
