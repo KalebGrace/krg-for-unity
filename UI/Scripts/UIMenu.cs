c89e6256b8f03e65309175e4208e14da2f6225cd
k76 <krg0077++src@gmail.com>
2020-09-18 01:06:21 -0700

    More work on UI menu

--- UI/Scripts/UIMenu.cs
+++ UI/Scripts/UIMenu.cs
@@ -1,9 +1,14 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace KRG
 {
     public class UIMenu : MonoBehaviour
     {
-        
+        // SERIALIZED FIELDS
+
+        public ScrollRect ScrollView;
+        public GameObject MenuItem;
+        public Vector3 ItemOffset;
     }
 }
