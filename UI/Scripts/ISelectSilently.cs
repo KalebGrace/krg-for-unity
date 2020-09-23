bdbf010a1d56174c5d4d3d75ae362ced277982c6
k76 <krg0077++src@gmail.com>
2020-09-23 05:55:29 -0700

    Complete working UI menu

---
+++ UI/Scripts/ISelectSilently.cs
@@ -0,0 +1,7 @@
+﻿namespace KRG
+{
+    public interface ISelectSilently
+    {
+        void SelectSilently();
+    }
+}
--- UI/Scripts/UIMenu.cs
+++ UI/Scripts/UIMenu.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 namespace KRG
@@ -10,5 +13,81 @@ namespace KRG
         public ScrollRect ScrollView;
         public GameObject MenuItem;
         public Vector3 ItemOffset;
+
+        // PRIVATE FIELDS
+
+        private List<Item> m_Items = new List<Item>();
+
+        // STRUCTS
+
+        public struct Item
+        {
+            public string Text;
+            public UnityAction OnClick;
+            public GameObject MenuItem;
+        }
+
+        // PUBLIC METHODS
+
+        public void Clear()
+        {
+            foreach (Item item in m_Items)
+            {
+                GameObject menuItem = item.MenuItem;
+                if (menuItem == MenuItem)
+                {
+                    menuItem.name = "Menu Item";
+                    menuItem.GetComponentInChildren<TextMeshProUGUI>().text = "Menu Item";
+
+                    Button button = menuItem.GetComponent<Button>();
+                    Button.ButtonClickedEvent clickedEvent = button.onClick;
+                    clickedEvent.RemoveAllListeners();
+                }
+                else
+                {
+                    Destroy(menuItem);
+                }
+            }
+
+            m_Items.Clear();
+        }
+
+        public void AddItem(string text, UnityAction onClick)
+        {
+            GameObject menuItem;
+            if (m_Items.Count == 0)
+            {
+                menuItem = MenuItem;
+            }
+            else
+            {
+                menuItem = Instantiate(MenuItem, MenuItem.transform.parent);
+                RectTransform rt = menuItem.GetComponent<RectTransform>();
+                rt.localPosition += ItemOffset * m_Items.Count;
+            }
+
+            menuItem.name = text;
+            menuItem.GetComponentInChildren<TextMeshProUGUI>().text = text;
+
+            Button button = menuItem.GetComponent<Button>();
+            Button.ButtonClickedEvent clickedEvent = button.onClick;
+            clickedEvent.RemoveAllListeners();
+            clickedEvent.AddListener(onClick);
+
+            m_Items.Add(new Item { Text = text, OnClick = onClick, MenuItem = menuItem });
+        }
+
+        public void SelectDefaultItem()
+        {
+            ISelectSilently iss = MenuItem.GetComponent<ISelectSilently>();
+            if (iss != null)
+            {
+                iss.SelectSilently();
+            }
+            else
+            {
+                MenuItem.GetComponent<Button>().Select();
+            }
+        }
     }
 }
