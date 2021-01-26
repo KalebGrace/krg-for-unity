4f92bd0717e3c729a644b70432bd14ad9b1ea172
k76 <krg0077++src@gmail.com>
2021-01-26 06:20:17 -0800

    Remove unused ScaleTexture method from GalleryCharacter

--- Character/Scripts/GalleryCharacter.cs
+++ Character/Scripts/GalleryCharacter.cs
@@ -25,11 +25,6 @@ namespace KRG
             m_Renderer = GetComponent<MeshRenderer>();
         }
 
-        public void ScaleTexture(float v)
-        {
-            transform.localScale = new Vector3(v, v, 1);
-        }
-
         public void SetKnown(bool isKnown)
         {
             Material.color = Material.color.SetAlpha(isKnown ? 1 : 0.2f);
@@ -38,7 +33,6 @@ namespace KRG
         public void UpdateTexture(Texture2D characterTexture)
         {
             Material.mainTexture = characterTexture;
-            ScaleTexture(1);
         }
     }
 }
